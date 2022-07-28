54a63f5846908473fe9610f6c78874e841ed55a3
DESKTOP-NNQKKTB\tokub <tokubou906@yahoo.co.jp>
2022-07-28 12:16:48 +0900

    クエリ内データをmapでレストランカードに展開

--- frontend/components/RestaurantsList/index.js
+++ frontend/components/RestaurantsList/index.js
@@ -20,46 +20,58 @@ const query = gql`
 const RestaurantList = () => {
     //graphqlでクエリ内のデータ取得
     const { loading, error, data } = useQuery(query)
-    console.log(data)
+    if (loading) return <h1>Loading...</h1>
+    if (error) return <h1>Error!!</h1>
+    //データがあればhtmlを出力
+    if (data) {
+        //データをmapで展開
+        return (
+            <Row>
+                {data.restaurants.map((res) => (
+                    <Col xs="6" sm="4" key={res.id}>
+                        <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+                            <CardImg
+                                src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
+                                top={true} style={{ height: 250 }} />
+                            <CardBody>
+                                <CardTitle>{res.name}</CardTitle>
+                                <CardTitle>{res.description}</CardTitle>
+                            </CardBody>
+                            <div className="card-footer">
+                                <Link
+                                    href={`restaurants/${res.id}`}
+                                    as={`restaurants?id=${res.id}`}
+                                >
+                                    <a className="btn btn-primary">もっと見る</a>
+                                </Link>
+                            </div>
+                        </Card>
+                    </Col>
+                ))}
+                <style jsx>
+                    {`
+                    a {
+                        color: white;
+                    }
+                    a:link {
+                        text-decoration: none;
+                        color: white;
+                    }
+                    a:hover {
+                        color: white
+                    }
+                    .card-colums {
+                        column-connt: 3;
+                    }
+                    `}
+                </style>
+            </Row>
+        );
+    } else {
+        <h1>レストランが見つかりませんでした。</h1>
+    }
 
-    return (
-        <Row>
-            <Col xs="6" sm="4">
-                <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-                    <CardImg
-                        src="http://localhost:1337/uploads/thumbnail_holo_23579976e1.jpg"
-                        top={true} style={{ height: 250 }} />
-                    <CardBody>
-                        <CardTitle>peko</CardTitle>
-                        <CardTitle>ぺこらのレストランです</CardTitle>
-                    </CardBody>
-                    <div className="card-footer">
-                        <Link href="/restaurants?id=1" as="/restaurants/1">
-                            <a className="btn btn-primary">もっと見る</a>
-                        </Link>
-                    </div>
-                </Card>
-            </Col>
 
-            <style jsx>
-                {`
-                a {
-                    color: white;
-                }
-                a:link {
-                    text-decoration: none;
-                    color: white;
-                }
-                a:hover {
-                    color: white
-                }
-                .card-colums {
-                    column-connt: 3;
-                }
-                `}
-            </style>
-        </Row>
-    );
 }
 
 export default RestaurantList;
