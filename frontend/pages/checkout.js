c269f22b480d6727028f86497e012a1730c98355
DESKTOP-NNQKKTB\tokub <tokubou906@yahoo.co.jp>
2022-07-30 16:28:31 +0900

    CardSection.js内容記述 reactststripeインストール(npm i @stripe/react-stripe-js) stripeインストール(npm i @stripe/stripe-js) checkoutjs修正

--- frontend/pages/checkout.js
+++ frontend/pages/checkout.js
@@ -1,8 +1,14 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { Col, Row } from "reactstrap";
 import Cart from "../components/Cart";
 import CheckOutForm from "../components/Checkout/CheckOutForm";
 
 const checkout = () => {
+  //公開可能キーを入力
+  const stripePromise = loadStripe(
+    "pk_test_51LR5F1HmHEVRGKagO9wwfJmkiNnTelTE7MjtQ2ZhdoAjkg80TGhmjGzadgILEKU9GMAoamVf3rFL5aAJPVFAgtN500mRCN7Ytk"
+  );
   return (
     <Row>
       <Col style={{ paddingRight: 0 }} sm={{ size: 3, oreder: 1, offset: 2 }}>
@@ -12,7 +18,10 @@ const checkout = () => {
         <Cart />
       </Col>
       <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
-        <CheckOutForm />
+        {/* チェックアウトフォームをElementsで囲ってstripe属性にstripePromiseを渡す */}
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
       </Col>
     </Row>
   );
